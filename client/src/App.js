51edddfaabaca87b0e64317a85e309e79db0d723
Ninamma <ninamma.rai@outlook.com>
2020-03-23 21:55:10 +0000

    Testing making request to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [apiResponse, setApiResponse] = useState("");
+
+  function callAPI() {
+    fetch("http://localhost:9000/testServer")
+    .then(res => res.text())
+    .then(res => setApiResponse(res))
+  }
+
+  callAPI();
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +18,7 @@ function App() {
           Welcome to the soon-to-be weather app! <span role="img" aria-label="sun behind rain cloud">🌦</span>
         </h1>
       </header>
+      <p className="App-intro">{apiResponse}</p>
     </div>
   );
 }
