daac09018e50a46e3b6f5ab0daf055201c1815cf
Ninamma <ninamma.rai@outlook.com>
2020-03-23 21:51:23 +0000

    Adding dotenv, cors and compare route

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var compareRouter = require('./routes/compare')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/compare', compareRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
