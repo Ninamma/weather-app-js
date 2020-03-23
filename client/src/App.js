b92c9c1683102fb5e14aa2396bc96e0ca2f6b100
Ninamma <ninamma.rai@outlook.com>
2020-03-23 17:13:27 +0000

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Welcome to the soon-to-be weather app! <span role="img" aria-label="sun behind rain cloud">🌦</span>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
